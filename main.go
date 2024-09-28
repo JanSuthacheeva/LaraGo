abff8c1d2b9d4c8f404b435338f2d367e3da6aee
Jan Steikowski <j.steikowski@aol.com>
2024-09-28 10:52:42 +0700

    Move functions to setup.go

--- main.go
+++ main.go
@@ -6,12 +6,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 )
 
 func main() {
-  sail := flag.Bool("sail", false, "If set, sail will be installed during the setup process.")
+  // sail := flag.Bool("sail", false, "If set, sail will be installed during the setup process.")
   phpStanLvl := flag.Int("phpstan-lvl", 9, "Set the phpstan level. Defaults to 9.")
 
   if len(os.Args) < 2 {
@@ -63,98 +62,3 @@ func main() {
     os.Exit(1)
   }
 }
-
-/**
- * Installs a new laravel project with the laravel new command.
- */
-func installLaravel(projectName string) error {
-  cmd := exec.Command("laravel", "new", projectName)
-  cmd.Stdin = os.Stdin
-  cmd.Stdout = os.Stdout
-  cmd.Stderr = os.Stderr
-
-  return cmd.Run()
-}
-
-/**
- *Installs all the dependencies into the new laravel project.
- */
-func installDependencies() error {
-  dependencies := []string{
-    "composer require --dev laravel/pint",
-    "composer require --dev phpstan/phpstan",
-    "composer require --dev larastan/larastan:^2.0",
-  }
-
-  for _, dep := range dependencies {
-    cmd := exec.Command("sh", "-c", dep)
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-
-    if err := cmd.Run(); err != nil {
-      return err
-    }
-  }
-
-  return nil
-}
-
-/**
- * Creates and writes the pre-commit.sample file in the
- * new laravel project.
- */
-func writePreCommitFile() error {
-  file, err := os.Create("pre-commit.sample")
-  if err != nil {
-    return err
-  }
-
-  _, err = file.WriteString(GetPreCommitFileContent())
-  if err != nil {
-    file.Close()
-    return err
-  }
-
-  err = file.Close()
-  if err != nil {
-    return err
-  }
-
-  return nil
-}
-
-/**
- * Creates a symbolic link between the pre-commit.sample file
- * in the project directory and the pre-commit file in the .git/hooks directory.
- */
-func createSymbolicLink() error {
-  if err := os.Chmod("pre-commit.sample", 777); err != nil {
-    return err
-  }
-  if err := os.Symlink("../../pre-commit.sample", ".git/hooks/pre-commit"); err != nil {
-    return err
-  }
-  return nil
-}
-
-func writePhpStanFile(phpstanLvl *int) error {
-  file, err := os.Create("phpstan.neon")
-  if err != nil {
-    return err
-  }
-
-  _, err = file.WriteString(GetPhpStanFileContent(phpstanLvl))
-  if err != nil {
-    file.Close()
-    return err
-  }
-
-  err = file.Close()
-  if err != nil {
-    return err
-  }
-
-  return nil
-}
-
---
+++ setup.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+  "os"
+  "os/exec"
+)
+
+/**
+ * Installs a new laravel project with the laravel new command.
+ */
+func installLaravel(projectName string) error {
+  cmd := exec.Command("laravel", "new", projectName)
+  cmd.Stdin = os.Stdin
+  cmd.Stdout = os.Stdout
+  cmd.Stderr = os.Stderr
+
+  return cmd.Run()
+}
+
+/**
+ *Installs all the dependencies into the new laravel project.
+ */
+func installDependencies() error {
+  dependencies := []string{
+    "composer require --dev laravel/pint",
+    "composer require --dev phpstan/phpstan",
+    "composer require --dev larastan/larastan:^2.0",
+  }
+
+  for _, dep := range dependencies {
+    cmd := exec.Command("sh", "-c", dep)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+
+    if err := cmd.Run(); err != nil {
+      return err
+    }
+  }
+
+  return nil
+}
+
+/**
+ * Creates and writes the pre-commit.sample file in the
+ * new laravel project.
+ */
+func writePreCommitFile() error {
+  file, err := os.Create("pre-commit.sample")
+  if err != nil {
+    return err
+  }
+
+  _, err = file.WriteString(GetPreCommitFileContent())
+  if err != nil {
+    file.Close()
+    return err
+  }
+
+  err = file.Close()
+  if err != nil {
+    return err
+  }
+
+  return nil
+}
+
+/**
+ * Creates a symbolic link between the pre-commit.sample file
+ * in the project directory and the pre-commit file in the .git/hooks directory.
+ */
+func createSymbolicLink() error {
+  if err := os.Chmod("pre-commit.sample", 777); err != nil {
+    return err
+  }
+  if err := os.Symlink("../../pre-commit.sample", ".git/hooks/pre-commit"); err != nil {
+    return err
+  }
+  return nil
+}
+
+func writePhpStanFile(phpstanLvl *int) error {
+  file, err := os.Create("phpstan.neon")
+  if err != nil {
+    return err
+  }
+
+  _, err = file.WriteString(GetPhpStanFileContent(phpstanLvl))
+  if err != nil {
+    file.Close()
+    return err
+  }
+
+  err = file.Close()
+  if err != nil {
+    return err
+  }
+
+  return nil
+}
+
